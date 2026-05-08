ece04d06371d8625222dd8a3ff64795ff21fc57c
Sandeep Regmi <sandeepregmi850@gmail.com>
2026-05-09 00:07:12 +0545

    fix: remove DB config from alembic.ini and move to env variables

--- alembic/env.py
+++ alembic/env.py
@@ -1,61 +1,22 @@
 from logging.config import fileConfig
-
 from sqlalchemy import engine_from_config, pool
 from alembic import context
 
-# ADD THIS 👇
-from app.database import DATABASE_URL, Base
-from app import models  # IMPORTANT: ensures models are loaded
+from dotenv import load_dotenv
+import os
 
-# Alembic Config object
+from app.database import Base
+from app import models  # ensures models are loaded
+
+load_dotenv()
+
 config = context.config
 
-DATABASE_URL = "postgresql://sandeep:S%%40ndeep07%%23@localhost:5432/notesdb"
+DATABASE_URL = os.getenv("DATABASE_URL")
+
 config.set_main_option("sqlalchemy.url", DATABASE_URL)
 
-# set DB URL dynamically (BEST PRACTICE)
-config.set_main_option("sqlalchemy.url", DATABASE_URL)
-
-# logging setup
 if config.config_file_name is not None:
     fileConfig(config.config_file_name)
 
-# IMPORTANT: metadata for autogenerate
 target_metadata = Base.metadata
-
-
-def run_migrations_offline():
-    url = config.get_main_option("sqlalchemy.url")
-
-    context.configure(
-        url=url,
-        target_metadata=target_metadata,
-        literal_binds=True,
-        dialect_opts={"paramstyle": "named"},
-    )
-
-    with context.begin_transaction():
-        context.run_migrations()
-
-
-def run_migrations_online():
-    connectable = engine_from_config(
-        config.get_section(config.config_ini_section),
-        prefix="sqlalchemy.",
-        poolclass=pool.NullPool,
-    )
-
-    with connectable.connect() as connection:
-        context.configure(
-            connection=connection,
-            target_metadata=target_metadata,
-        )
-
-        with context.begin_transaction():
-            context.run_migrations()
-
-
-if context.is_offline_mode():
-    run_migrations_offline()
-else:
-    run_migrations_online()
